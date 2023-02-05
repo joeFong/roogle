82a0deb7e983da9dfb802fd7c72bee419eb918f0
Joe Fong <joemanfong@gmail.com>
2023-02-05 10:05:31 -0800

    update

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,20 +5,19 @@ import qs from "qs";
 async function getData(props: any) {
     const params = qs.stringify({...props})
     const res = await fetch(`http://localhost:3000/api/search?${params}`);
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
+    // // The return value is *not* serialized
+    // // You can return Date, Map, Set, etc.
   
-    // Recommendation: handle errors
+    // // Recommendation: handle errors
     if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error('Failed to fetch data');
     }
-  
     return res.json();
+    // return { items: [] }
 }
 
 export default async function Search({ searchParams }: any) {
-  // const searchParams = useSearchParams();
   const res = await getData({
     ...searchParams
   })
